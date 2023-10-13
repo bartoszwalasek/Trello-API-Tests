4729b47730eedb96b2dc7c2149975322bce6f235
bartosz-walasek <b.wal1990@gmail.com>
2023-10-13 18:44:12 +0200

    create test - Delete a Board

--- tests/boards.test.js
+++ tests/boards.test.js
@@ -2,13 +2,26 @@ import { expect } from "chai";
 import pkg from "pactum";
 const { spec } = pkg;
 import { credentials } from "../helpers/credentials.js";
+import { BASE_URL } from "../helpers/data.js";
 
 describe("APi tests with Trello boards", () => {
+
+    let board
+
     it("Create a Board", async () => {
         const response = await spec()
-            .post("https://api.trello.com/1/boards/")
+            .post(BASE_URL)
             .withQueryParams({
-                name: "board to test",
+                name: "Board to test",
+                ...credentials
+            })
+        expect(response.statusCode).to.eql(200)
+        board = response.body
+    })
+    it("Delete a Board", async () => {
+        const response = await spec()
+            .delete(`${BASE_URL}/${board.id}`)
+            .withQueryParams({
                 ...credentials
             })
         expect(response.statusCode).to.eql(200)
