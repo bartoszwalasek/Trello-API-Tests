fef6fd1f2748328be875e43cf00322595e89ccb2
bartosz-walasek <b.wal1990@gmail.com>
2023-10-14 12:51:21 +0200

    create test - Create a List on a Board

--- helpers/data.js
+++ helpers/data.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://api.trello.com/1/boards/"
+export const BASE_URL = "https://api.trello.com/1/"
--- tests/boards.test.js
+++ tests/boards.test.js
@@ -7,10 +7,11 @@ import { BASE_URL } from "../helpers/data.js";
 describe("APi tests with Trello boards", () => {
 
     let board
+    let list
 
     it("Create a Board", async () => {
         const response = await spec()
-            .post(BASE_URL)
+            .post(`${BASE_URL}boards/`)
             .withQueryParams({
                 name: "Board to test",
                 ...credentials
@@ -19,9 +20,23 @@ describe("APi tests with Trello boards", () => {
         expect(response.body.name).to.eql("Board to test")
         board = response.body
     })
+    it("Create a List on a Board", async () => {
+        const response = await spec()
+            .post(`${BASE_URL}boards/${board.id}/lists`)
+            .withQueryParams({
+                name: "New List",
+                ...credentials
+            })
+            .withHeaders({
+                Accept: 'application/json'
+            })
+        expect(response.statusCode).to.eql(200)
+        expect(response.body.name).to.eql("New List")
+        list = response.body
+    })
     it("Delete a Board", async () => {
         const response = await spec()
-            .delete(`${BASE_URL}/${board.id}`)
+            .delete(`${BASE_URL}boards/${board.id}`)
             .withQueryParams({
                 ...credentials
             })
@@ -29,7 +44,7 @@ describe("APi tests with Trello boards", () => {
     })
     it("Get a Board after deleting", async () => {
         const response = await spec()
-            .get(`${BASE_URL}/${board.id}`)
+            .get(`${BASE_URL}boards/${board.id}`)
             .withQueryParams({
                 ...credentials
             })
