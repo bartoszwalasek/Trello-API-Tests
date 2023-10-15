4deda22e179c0aa44ace2fdeb164699474cf0e5d
bartosz-walasek <b.wal1990@gmail.com>
2023-10-15 17:50:57 +0200

    create test - Get the Members of Board

--- helpers/data.js
+++ helpers/data.js
@@ -1,2 +1,7 @@
 export const BASE_URL = "https://api.trello.com/1/"
 export let list_name = "New List"
+export let board_member = {
+    id: "63bc0cdade0b2d00defdc186",
+    fullName: "Bartosz Walasek",
+    username: "bartoszwalasek3"
+  }
