251cb570295bbb52a2968f257a819e1df29f969e
Aszton <bartoszkuczera88@gmail.com>
2023-10-15 20:37:25 +0200

    code_review

--- helpers/data.js
+++ helpers/data.js
@@ -1,7 +1,7 @@
-export const BASE_URL = "https://api.trello.com/1/"
-export let list_name = "New List"
-export let board_member = {
-    id: "63bc0cdade0b2d00defdc186",
-    fullName: "Bartosz Walasek",
-    username: "bartoszwalasek3"
-  }
+export const BASE_URL = "https://api.trello.com/1";
+export const list_name = "New List";
+export const board_member = {
+  id: "63bc0cdade0b2d00defdc186",
+  fullName: "Bartosz Walasek",
+  username: "bartoszwalasek3",
+};
