4df729abb02e9b24ba29262dfbf64f55e74acad8
bartosz-walasek <b.wal1990@gmail.com>
2023-10-18 19:55:24 +0200

    move board_member to credentials file

--- helpers/credentials.js
+++ helpers/credentials.js
@@ -4,3 +4,9 @@ export const credentials = {
     key: process.env.KEY,
     token: process.env.TOKEN,
 }
+
+export const board_member = {
+    id: process.env.ID,
+    fullName: "Bartosz Walasek",
+    username: "bartoszwalasek3"
+  }
