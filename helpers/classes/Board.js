7f5aed9cdad89d7ef46875b5479fc0580c55b1ab
bartosz-walasek <b.wal1990@gmail.com>
2024-04-20 10:03:18 +0200

    add try catch to board's methods

--- helpers/classes/Board.js
+++ helpers/classes/Board.js
@@ -12,75 +12,109 @@ export class Board {
   constructor() {}
 
   async createNewBoard(boardName) {
-    const response = await spec()
-      .post(`${BASE_URL}/boards/`)
-      .withQueryParams({
-        name: boardName,
-        ...credentials,
-      });
-    this.createdBoard = response.body;
-    return response;
+    try {
+      const response = await spec()
+        .post(`${BASE_URL}/boards/`)
+        .withQueryParams({
+          name: boardName,
+          ...credentials,
+        });
+      this.createdBoard = response.body;
+      return response;
+    } catch {
+      throw new Error("Failed to create a new board.");
+    }
   }
+
   async getBoard(board) {
-    const response = await spec()
-      .get(`${BASE_URL}/boards/${board.id}`)
-      .withQueryParams({
-        ...credentials,
-      });
-    return response;
+    try {
+      const response = await spec()
+        .get(`${BASE_URL}/boards/${board.id}`)
+        .withQueryParams({
+          ...credentials,
+        });
+      return response;
+    } catch {
+      throw new Error("Failed to get a board information.");
+    }
   }
+
   async createLabelOnBoard(board, labelName, labelColor) {
-    const response = await spec()
-      .post(`${BASE_URL}/boards/${board.id}/labels`)
-      .withQueryParams({
-        name: labelName,
-        color: labelColor,
-        ...credentials,
-      });
-    this.createdLabel = response.body;
-    return response;
+    try {
+      const response = await spec()
+        .post(`${BASE_URL}/boards/${board.id}/labels`)
+        .withQueryParams({
+          name: labelName,
+          color: labelColor,
+          ...credentials,
+        });
+      this.createdLabel = response.body;
+      return response;
+    } catch {
+      throw new Error("Failed to create a label.");
+    }
   }
+
   async deleteBoard(board) {
-    const response = await spec()
-      .delete(`${BASE_URL}/boards/${board.id}`)
-      .withQueryParams({
-        ...credentials,
-      });
-    return response;
+    try {
+      const response = await spec()
+        .delete(`${BASE_URL}/boards/${board.id}`)
+        .withQueryParams({
+          ...credentials,
+        });
+      return response;
+    } catch {
+      throw new Error("Failed to delete a board.");
+    }
   }
+
   async createListOnBoard(board, listName) {
-    const response = await spec()
-      .post(`${BASE_URL}/boards/${board.id}/lists`)
-      .withQueryParams({
-        name: listName,
-        ...credentials,
-      })
-      .withHeaders({
-        Accept: "application/json",
-      });
-    return response;
+    try {
+      const response = await spec()
+        .post(`${BASE_URL}/boards/${board.id}/lists`)
+        .withQueryParams({
+          name: listName,
+          ...credentials,
+        })
+        .withHeaders({
+          Accept: "application/json",
+        });
+      return response;
+    } catch {
+      throw new Error("Failed to create a list.");
+    }
   }
+
   async getListsOnBoard(board) {
-    const response = await spec()
-      .get(`${BASE_URL}/boards/${board.id}/lists`)
-      .withQueryParams({
-        ...credentials,
-      })
-      .withHeaders({
-        Accept: "application/json",
-      });
-    this.lists = response.body;
-    return response;
+    try {
+      const response = await spec()
+        .get(`${BASE_URL}/boards/${board.id}/lists`)
+        .withQueryParams({
+          ...credentials,
+        })
+        .withHeaders({
+          Accept: "application/json",
+        });
+      this.lists = response.body;
+      return response;
+    } catch {
+      throw new Error("Failed to get a list information.");
+    }
   }
+
   async getMembersOfBoard(board) {
-    const response = await spec()
-      .get(`${BASE_URL}/boards/${board.id}/members`)
-      .withQueryParams({
-        ...credentials,
-      });
-    this.boardMember = response.body.find(
-      (member) => member.id === boardMember.id
-    );
-    return response;
+    try {
+      const response = await spec()
+        .get(`${BASE_URL}/boards/${board.id}/members`)
+        .withQueryParams({
+          ...credentials,
+        });
+      this.boardMember = response.body.find(
+        (member) => member.id === boardMember.id
+      );
+      return response;
+    } catch {
+      throw new Error("Failed to get a members information.");
+    }
   }
 }
