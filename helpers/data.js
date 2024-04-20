23e337bea3030e02f8aa0b5ef22af55a69319bba
bartosz-walasek <b.wal1990@gmail.com>
2024-04-20 12:24:31 +0200

    refactor tests

--- helpers/data.js
+++ helpers/data.js
@@ -1,7 +1,1 @@
 export const BASE_URL = "https://api.trello.com/1/";
-export const fileName = "test.txt";
-export const dataToUpdateCard = {
-  name: "New Card after update",
-  desc: "New Desc after update",
-  idList: undefined,
-};
