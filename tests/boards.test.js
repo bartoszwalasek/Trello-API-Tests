81b515a98b753bb6805a1cbcbe802d3acd3e28f9
bartosz-walasek <b.wal1990@gmail.com>
2024-04-20 12:23:13 +0200

    refactor tests

--- tests/boards.test.js
+++ tests/boards.test.js
@@ -7,59 +7,74 @@ describe("API tests with Trello boards", () => {
   const board = new Board();
 
   it("Create a Board", async () => {
+    // ARRANGE
     const boardName = "New Board";
     const expectedStatusCode = 200;
 
+    // ACT
     const response = await board.createNewBoard(boardName);
 
+    // ASSERT
     expect(response.statusCode).to.eql(expectedStatusCode);
-    expect(board.createdBoard.name).to.eql(boardName);
+    expect(response.body.name).to.eql(boardName);
   });
 
   it("Get a Board after creating", async () => {
-    const statusCode = 200;
+    // ARRANGE
+    const expectedStatusCode = 200;
 
-    const response = await board.getBoard(board.createdBoard, statusCode);
+    // ACT
+    const response = await board.getBoard(board.createdBoard);
 
-    expect(response.statusCode).to.eql(statusCode);
+    // ASSERT
+    expect(response.statusCode).to.eql(expectedStatusCode);
   });
 
   it("Create a Label on a Board", async () => {
+    // ARRANGE
     const labelName = "New Label";
     const labelColor = "green";
     const expectedStatusCode = 200;
 
+    // ACT
     const response = await board.createLabelOnBoard(
       board.createdBoard,
       labelName,
       labelColor
     );
 
+    // ASSERT
     expect(response.statusCode).to.eql(expectedStatusCode);
     expect(response.body.name).to.eql(labelName);
     expect(response.body.color).to.eql(labelColor);
   });
 
   it("Create a List on a Board", async () => {
+    // ARRANGE
     const listName = "New List";
     const expectedStatusCode = 200;
 
+    // ACT
     const response = await board.createListOnBoard(
       board.createdBoard,
       listName
     );
 
+    // ASSERT
     expect(response.statusCode).to.eql(expectedStatusCode);
     expect(response.body.name).to.eql(listName);
   });
 
   it("Get the Lists on a Board", async () => {
+    // ARRANGE
     const list = new List();
     const expectedLists = ["To Do", "Doing", "Done", "New List"];
     const expectedStatusCode = 200;
 
+    // ACT
     const response = await board.getListsOnBoard(board.createdBoard);
 
+    // ASSERT
     expect(response.statusCode).to.eql(expectedStatusCode);
     expect(response.body.length).to.eql(expectedLists.length);
     expectedLists.forEach((expectedListName) => {
@@ -68,23 +83,21 @@ describe("API tests with Trello boards", () => {
     });
   });
 
-  it("Find a newly added List", () => {
-    const list = new List();
-    const expectedListName = "New List";
-
-    const newList = list.findList(board.lists, expectedListName);
-
-    expect(newList).to.include({ name: expectedListName });
-  });
-
   it("Verify the Member of Board", async () => {
+    // ARRANGE
     const expectedMember = boardMember;
     const expectedStatusCode = 200;
 
+    // ACT
     const response = await board.getMembersOfBoard(board.createdBoard);
 
+    // ASSERT
+    const desiredMember = response.body.find(
+      (member) => member.id === expectedMember.id
+    );
+
     expect(response.statusCode).to.eql(expectedStatusCode);
-    expect(board.boardMember).to.eql({
+    expect(desiredMember).to.eql({
       id: expectedMember.id,
       fullName: expectedMember.fullName,
       username: expectedMember.username,
@@ -92,18 +105,24 @@ describe("API tests with Trello boards", () => {
   });
 
   it("Delete a Board", async () => {
+    // ARRANGE
     const expectedStatusCode = 200;
 
+    // ACT
     const response = await board.deleteBoard(board.createdBoard);
 
+    // ASSERT
     expect(response.statusCode).to.eql(expectedStatusCode);
   });
 
   it("Get a Board after deleting", async () => {
+    // ARRANGE
     const expectedStatusCode = 404;
 
+    // ACT
     const response = await board.getBoard(board.createdBoard);
 
+    // ASSERT
     expect(response.statusCode).to.eql(expectedStatusCode);
   });
 });
--- tests/cards.test.js
+++ tests/cards.test.js
@@ -1,48 +1,168 @@
-import { board } from "../helpers/classes/Board.js";
-import { card } from "../helpers/classes/Card.js";
-import { list } from "../helpers/classes/List.js";
-import { defaultLists, dataToUpdateCard } from "../helpers/data.js";
+import { Board } from "../helpers/classes/Board.js";
+import { Card } from "../helpers/classes/Card.js";
+import { Date } from "../helpers/classes/Date.js";
+import { List } from "../helpers/classes/List.js";
+import { boardMember } from "../helpers/credentials.js";
+import { expect } from "chai";
 
 describe("API tests with Trello cards", () => {
+  const board = new Board();
+  const card = new Card();
+
   before("Create a New Board to conduct tests with Cards", async () => {
     await board.createNewBoard("New Board to Card test");
-    await board.getBoard(board.createdBoard, 200);
     await board.getListsOnBoard(board.createdBoard);
   });
 
   after("Delete the New Board after tests with Cards", async () => {
     await board.deleteBoard(board.createdBoard);
-    await board.getBoard(board.createdBoard, 404);
   });
 
   it("Create a New Card in To Do List", async () => {
-    let toDoList = list.findList(board.lists, defaultLists[0].name);
-    await card.createNewCard(toDoList, "New Card", "Test Description");
+    // ARRANGE
+    const list = new List();
+    const date = new Date();
+
+    const currentTime = date.getCurrentUTCTime();
+    const toDoList = list.findList(board.lists, "To Do");
+    const cardName = "New Card";
+    const cardDescription = "Test Description";
+    const expectedStatusCode = 200;
+
+    // ACT
+    const response = await card.createNewCard(
+      toDoList,
+      cardName,
+      cardDescription
+    );
+
+    // ASSERT
+    const compareDatesStatus = date.compareDates(
+      response.body.dateLastActivity,
+      currentTime
+    );
+
+    expect(response.statusCode).to.eql(expectedStatusCode);
+    expect(response.body.idList).to.eql(toDoList.id);
+    expect(response.body.name).to.eql(cardName);
+    expect(response.body.desc).to.eql(cardDescription);
+    expect(compareDatesStatus).to.be.true;
   });
 
   it("Get a Card after creating", async () => {
-    await card.getCard(card.createdCard, 200);
+    // ARRANGE
+    const expectedStatusCode = 200;
+
+    // ACT
+    const response = await card.getCard(card.createdCard);
+
+    // ASSERT
+    expect(response.statusCode).to.eql(expectedStatusCode);
   });
 
   it("Add a new comment to a Card", async () => {
-    await card.addNewCommentToCard(card.createdCard, "New comment");
+    // ARRANGE
+    const date = new Date();
+
+    const currentTime = date.getCurrentUTCTime();
+    const commentName = "New Comment";
+    const expectedStatusCode = 200;
+
+    // ACT
+    const response = await card.addNewCommentToCard(
+      card.createdCard,
+      commentName
+    );
+
+    // ASSERT
+    const compareDatesStatus = date.compareDates(response.body.date, currentTime);
+
+    expect(response.statusCode).to.eql(expectedStatusCode);
+    expect(response.body.data.text).to.eql(commentName);
+    expect(response.body.data.card.id).to.eql(card.createdCard.id);
+    expect(response.body.idMemberCreator).to.eql(boardMember.id);
+    expect(compareDatesStatus).to.be.true;
   });
 
   it("Move a Card to Doing List", async () => {
-    let doingList = list.findList(board.lists, defaultLists[1].name);
-    dataToUpdateCard.idList = doingList.id;
-    await card.updateCard(card.createdCard, dataToUpdateCard);
+    // ARRANGE
+    const list = new List();
+    const date = new Date();
+
+    const currentTime = date.getCurrentUTCTime();
+    const doingList = list.findList(board.lists, "Doing");
+    const dataToUpdateCard = {
+      name: "New Card after update",
+      desc: "New Desc after update",
+      idList: doingList.id,
+    };
+    const expectedStatusCode = 200;
+
+    // ACT
+    const response = await card.updateCard(card.createdCard, dataToUpdateCard);
+
+    // ASSERT
+    const compareDatesStatus = date.compareDates(
+      response.body.dateLastActivity,
+      currentTime
+    );
+
+    expect(response.statusCode).to.eql(expectedStatusCode);
+    expect(response.body.name).to.eql(dataToUpdateCard.name);
+    expect(response.body.desc).to.eql(dataToUpdateCard.desc);
+    expect(response.body.idList).to.eql(dataToUpdateCard.idList);
+    expect(compareDatesStatus).to.be.true;
   });
 
   it("Create an Attachment on Card", async () => {
-    await card.createAttachmentOnCard(card.createdCard, "New attachment");
+    // ARRANGE
+    const date = new Date();
+
+    const currentTime = date.getCurrentUTCTime();
+    const attachmentName = "New Comment";
+    const fileName = "test.txt";
+    const expectedStatusCode = 200;
+
+    // ACT
+    const response = await card.createAttachmentOnCard(
+      card.createdCard,
+      attachmentName,
+      fileName
+    );
+
+    // ASSERT
+    const compareDatesStatus = date.compareDates(response.body.date, currentTime);
+
+    expect(response.statusCode).to.eql(expectedStatusCode);
+    expect(response.body.name).to.eql(attachmentName);
+    expect(response.body.fileName).to.eql(fileName);
+    expect(response.body.url).to.eql(
+      `https://trello.com/1/cards/${card.createdCard.id}/attachments/${response.body.id}/download/${fileName}`
+    );
+    expect(response.body.idMember).to.eql(boardMember.id);
+    expect(response.body.bytes).to.not.eql(0);
+    expect(compareDatesStatus).to.be.true;
   });
 
   it("Delete a Card", async () => {
-    await card.deleteCard(card.createdCard);
+    // ARRANGE
+    const expectedStatusCode = 200;
+
+    // ACT
+    const response = await card.deleteCard(card.createdCard);
+
+    // ASSERT
+    expect(response.statusCode).to.eql(expectedStatusCode);
   });
 
   it("Get a Card after deleting", async () => {
-    await card.getCard(card.createdCard, 404);
+    // ARRANGE
+    const expectedStatusCode = 404;
+
+    // ACT
+    const response = await card.getCard(card.createdCard);
+
+    // ASSERT
+    expect(response.statusCode).to.eql(expectedStatusCode);
   });
 });
